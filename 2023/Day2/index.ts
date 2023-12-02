8d36b8066576540b6ea5b33b2067a0551b8ca2ae
Jerred Shepherd <github@sjer.red>
2023-12-02 11:09:42 -0800

    init day 2

---
+++ 2023/Day2/index.ts
@@ -0,0 +1,11 @@
+import assert from "assert";
+import { _cache } from "module";
+import * as R from "remeda";
+
+export async function solvePartOne(file: string): Promise<number> {
+  const contents = await Bun.file(file).text();
+}
+
+export async function solvePartTwo(file: string): Promise<number> {
+  const contents = await Bun.file(file).text();
+}
--- 2023/README.md
+++ 2023/README.md
@@ -7,3 +7,4 @@ My solutions to Advent of Code 2023 written in TypeScript.
 |                     Day                      | Part 1 | Part 2 |
 | :------------------------------------------: | :----: | :----: |
 | [Day 1](https://adventofcode.com/2023/day/1) |   ⭐   |   ⭐   |
+| [Day 1](https://adventofcode.com/2023/day/2) |      |      |
